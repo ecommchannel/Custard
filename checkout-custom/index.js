58c9e80897f9f4091643f1135bcf619eeecf79f9
Kyle2911 <Kyle.delacruz@goldenabc.com>
2023-09-13 11:29:32 +0800

    add

--- checkout-custom/index.js
+++ checkout-custom/index.js
@@ -1,7 +1,4 @@
 import { Custard } from "@discolabs/custard-js";
 import { changeMarketingText } from './marketing';
-import { pickupstore } from './pickupstore';
 
 window.custard = new Custard([changeMarketingText]);
-window.custard = new Custard([pickupstore]);
-
