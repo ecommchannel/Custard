951a649b7aca6064c8f15d281dded8b47ad13d5b
Kyle2911 <Kyle.delacruz@goldenabc.com>
2023-09-14 09:55:02 +0800

    add

--- checkout-custom/pickuplist.js
+++ checkout-custom/pickuplist.js
@@ -11,7 +11,7 @@ export class pickUpList extends CustardModule {
     }
     setup() {
 
-        await this.$element.find('.section__content').html(this.options.html_templates.pickup_list);
+        this.$element.find('.section__content').html(this.options.html_templates.pickup_list);
 
 
     }
