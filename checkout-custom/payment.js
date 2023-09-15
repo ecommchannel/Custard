d833c3b97c2fdc574ce4db9556f2c83c60a1e449
Kyle2911 <Kyle.delacruz@goldenabc.com>
2023-09-15 11:01:11 +0800

    add

--- checkout-custom/payment.js
+++ checkout-custom/payment.js
@@ -10,15 +10,9 @@ export class Payment extends CustardModule {
         return '[data-step="' + STEP_PAYMENT_METHOD + '"]';
     }
     setup() {
-        arrData = [];
-        for (let i = 0; i < document.getElementsByClassName("review-block__label").length; i++) {
-            htmlData = document.getElementsByClassName("review-block__label")[i].innerText;
-            arrData.push(htmlData);
-        }
-
-        if (arrData.includes('Ship to') != true) {
+     
             this.$element.find('.review-block__content').append(this.options.html_templates.pickupReviewDetailsBlock);
-        }
+   
 
     }
 }
