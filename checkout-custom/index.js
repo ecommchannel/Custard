10b90f37257da5fabcc351884d40cafa42062650
Kyle2911 <Kyle.delacruz@goldenabc.com>
2023-09-15 10:25:52 +0800

    add

--- checkout-custom/index.js
+++ checkout-custom/index.js
@@ -2,5 +2,6 @@ import { Custard } from "@discolabs/custard-js";
 import { changeMarketingText } from './marketing';
 import { removeExisting } from './removeExistingLocaton';
 import { pickUpList } from './pickuplist';
+import { Payment } from './payment';
 
-window.custard = new Custard([changeMarketingText, removeExisting, pickUpList]);
+window.custard = new Custard([changeMarketingText, removeExisting, pickUpList, Payment]);
---
+++ checkout-custom/payment.js
@@ -0,0 +1,17 @@
+import { CustardModule, STEP_PAYMENT_METHOD } from "@discolabs/custard-js";
+export class Payment extends CustardModule {
+    id() {
+        return 'payment';
+    }
+    steps() {
+        return [STEP_PAYMENT_METHOD];
+    }
+    selector() {
+        return '[data-step="' + STEP_PAYMENT_METHOD + '"]';
+    }
+    setup() {
+        this.$element.find('.review-block__content').append(this.options.html_templates.pickupReviewDetailsBlock);
+
+    }
+}
+
